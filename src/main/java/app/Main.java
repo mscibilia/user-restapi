5bad67b42b8ab0b3d86bb651936de4505204ac00
Scibs <mscibilia91@gmail.com>
2018-03-10 09:46:56 +0800

    Replaced lambda expressions used for Route implementatuibs in Main with request handlers.

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -11,36 +11,21 @@ import com.google.gson.Gson;
 
 import app.model.User;
 import app.persistence.UserHelper;
+import app.requesthandler.CreateUserRequestHandler;
+import app.requesthandler.DeleteUserRequestHandler;
+import app.requesthandler.GetAllUsersRequestHandler;
+import app.requesthandler.GetUserRequestHandler;
+import app.requesthandler.UpdateUserRequestHandler;
 
 public class Main {
 	
 	
     public static void main(String[] args) {
         
-        get("/users", (req, res) -> {
-        	Gson gson = new Gson();
-        	res.type("application/json");
-        	res.status(200);
-        	return gson.toJson(UserHelper.getInstance().getUserList());
-        });
+        get("/users", new GetAllUsersRequestHandler());
         
         
-		get("/users/:id", (req, res) -> {
-			Integer idToSearch = Integer.valueOf(req.params(":id"));
-			User user = UserHelper.getInstance().getUserById(idToSearch);
-			
-			if (user != null)	{
-				Gson gson = new Gson();
-				res.type("application/json");
-				res.status(200);
-				return gson.toJson(user);
-				
-			}	else	{
-				res.status(400);
-				return "User with specified ID not found";
-			}
-			
-		});
+		get("/users/:id", new GetUserRequestHandler());
 		
 		//Either implement this cleaner or get rid of it, Does it add value?
 		get("/users/byName/:name", (req, res) -> {
@@ -61,53 +46,11 @@ public class Main {
 		});
 
 		
-		post("/users/create", "application/json", (req, res) -> {
-			Gson gson = new Gson();
-			String responseMessage = null;
-			User incomingUser = gson.fromJson(req.body(), User.class);
-			try {
-				UserHelper.getInstance().addUser(incomingUser);
-				res.status(201);
-				responseMessage = "User creation successful: " + incomingUser.toString();
-			} catch (Exception e) {
-				res.status(500);
-				responseMessage = "User creation failed: Server Error";
-			}
-
-			return responseMessage;
-
-		});
+		post("/users/create", "application/json", new CreateUserRequestHandler());
 		
-		put("/users/update","application/json", (req, res) -> {
-			Gson gson = new Gson();
-			String responseMessage = null;
-			User incomingUser = gson.fromJson(req.body(), User.class);
-			if (UserHelper.getInstance().updateUser(incomingUser)) {
-				res.status(200);
-				responseMessage = "User update successful" + incomingUser.toString();
-			} else {
-				res.status(400);
-				responseMessage = "User update failed: User not found";
-			}
-
-			return responseMessage;
-
-		});
+		put("/users/update","application/json", new UpdateUserRequestHandler());
 		
-		delete("/users/:id", (req, res) -> {
-			Integer idToDelete = Integer.valueOf(req.params(":id"));
-			
-			String responseMessage = null;
-			if (UserHelper.getInstance().deleteUser(idToDelete)) {
-				res.status(200);
-				responseMessage = "User deleted successfully";
-			} else {
-				res.status(404);
-				responseMessage = "User with specified ID not found";
-			}
-			
-			return responseMessage;
-		});
+		delete("/users/:id", new DeleteUserRequestHandler());
     }
     
     

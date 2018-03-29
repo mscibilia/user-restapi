1cb0c09e93d6818059f303f26c33448699c06799
Scibs <mscibilia91@gmail.com>
2018-03-29 22:24:28 +0800

    Added test classes for DeleteUserRequestHandler and UpdateUserRequestHandler.

---
+++ src/test/java/app/requesthandler/DeleteUserRequestHandlerTest.java
@@ -0,0 +1,45 @@
+package app.requesthandler;
+
+import static org.junit.Assert.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import app.persistence.UserHelper;
+
+public class DeleteUserRequestHandlerTest {
+
+	@Test
+	public void processShouldReturnSuccessCodeGivenUserDeletedSuccessfully() {
+		UserHelper mockUserHelper = Mockito.mock(UserHelper.class);
+		int idToDelete = 1;
+		Mockito.when(mockUserHelper.deleteUser(idToDelete)).thenReturn(true);
+		
+		DeleteUserRequestHandler sut = new DeleteUserRequestHandler(mockUserHelper);
+		
+		Map<String, String> urlParams = new HashMap<>();
+		urlParams.put(":id", String.valueOf(idToDelete));
+		Answer result  = sut.process(new EmptyRequestPayload(), urlParams);
+		
+		assertEquals(200, result.getHttpCode());
+	}
+	
+	@Test
+	public void processShouldReturnNotFoundCodeGivenUserDeletionFailed() {
+		UserHelper mockUserHelper = Mockito.mock(UserHelper.class);
+		int idToDelete = 1;
+		Mockito.when(mockUserHelper.deleteUser(idToDelete)).thenReturn(false);
+		
+		DeleteUserRequestHandler sut = new DeleteUserRequestHandler(mockUserHelper);
+		
+		Map<String, String> urlParams = new HashMap<>();
+		urlParams.put(":id", String.valueOf(idToDelete));
+		Answer result  = sut.process(new EmptyRequestPayload(), urlParams);
+		
+		assertEquals(404, result.getHttpCode());
+	}
+
+}

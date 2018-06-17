7c06f8854de27d97ab2f67a81aded2bea71f30ff
Scibs <mscibilia91@gmail.com>
2018-06-17 20:42:44 +0800

    Added spring bean config for all request handlers

--- src/main/java/app/AppConfig.java
+++ src/main/java/app/AppConfig.java
@@ -6,6 +6,11 @@ import org.springframework.context.annotation.Scope;
 
 import app.persistence.HibernateUserHelper;
 import app.persistence.UserHelper;
+import app.requesthandler.CreateUserRequestHandler;
+import app.requesthandler.DeleteUserRequestHandler;
+import app.requesthandler.GetAllUsersRequestHandler;
+import app.requesthandler.GetUserRequestHandler;
+import app.requesthandler.UpdateUserRequestHandler;
 
 @Configuration
 public class AppConfig {
@@ -14,6 +19,36 @@ public class AppConfig {
 	@Scope("singleton")
 	public UserHelper getUserHelper()	{
 		return new HibernateUserHelper(new org.hibernate.cfg.Configuration().configure().buildSessionFactory());
+	}
+	
+	@Bean(name="getAllUsersRequestHandler")
+	@Scope("prototype")
+	public GetAllUsersRequestHandler getGetAllUsersRequestHandler()	{
+		return new GetAllUsersRequestHandler(getUserHelper());
+	}
+	
+	@Bean(name="getUserRequestHandler")
+	@Scope("prototype")
+	public GetUserRequestHandler getGetUserRequestHandler()	{
+		return new GetUserRequestHandler(getUserHelper());
+	}
+	
+	@Bean(name="createUserRequestHandler")
+	@Scope("prototype")
+	public CreateUserRequestHandler getCreateUserRequestHandler()	{
+		return new CreateUserRequestHandler(getUserHelper());
+	}
+	
+	@Bean(name="updateUserRequestHandler")
+	@Scope("prototype")
+	public UpdateUserRequestHandler getUpdateUserRequestHandler()	{
+		return new UpdateUserRequestHandler(getUserHelper());
+	}
+	
+	@Bean(name="deleteUserRequestHandler")
+	@Scope("prototype")
+	public DeleteUserRequestHandler getDeleteUserRequestHandler()	{
+		return new DeleteUserRequestHandler(getUserHelper());
 	}
 	
 }

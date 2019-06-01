c412035e4339b2a841fdfe72d387ca7533eef295
Scibs <mscibilia91@gmail.com>
2019-06-01 13:59:16 +0800

    Removed old implementation of User DAO and added spring jpa repository.

--- src/main/java/app/AppConfig.java
+++ src/main/java/app/AppConfig.java
@@ -1,11 +1,12 @@
 package app;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Scope;
 
-import app.persistence.HibernateUserHelper;
-import app.persistence.UserHelper;
+import app.repository.UserRepository;
 import app.requesthandler.CreateUserRequestHandler;
 import app.requesthandler.DeleteUserRequestHandler;
 import app.requesthandler.GetAllUsersRequestHandler;
@@ -13,42 +14,40 @@ import app.requesthandler.GetUserRequestHandler;
 import app.requesthandler.UpdateUserRequestHandler;
 
 @Configuration
+@Import(JpaConfig.class)
 public class AppConfig {
 
-	@Bean(name="userHelper")
-	@Scope("singleton")
-	public UserHelper getUserHelper()	{
-		return new HibernateUserHelper(new org.hibernate.cfg.Configuration().configure().buildSessionFactory());
-	}
+	@Autowired
+	private UserRepository userRepository;
 	
 	@Bean(name="getAllUsersRequestHandler")
 	@Scope("prototype")
 	public GetAllUsersRequestHandler getGetAllUsersRequestHandler()	{
-		return new GetAllUsersRequestHandler(getUserHelper());
+		return new GetAllUsersRequestHandler(userRepository);
 	}
 	
 	@Bean(name="getUserRequestHandler")
 	@Scope("prototype")
 	public GetUserRequestHandler getGetUserRequestHandler()	{
-		return new GetUserRequestHandler(getUserHelper());
+		return new GetUserRequestHandler(userRepository);
 	}
 	
 	@Bean(name="createUserRequestHandler")
 	@Scope("prototype")
 	public CreateUserRequestHandler getCreateUserRequestHandler()	{
-		return new CreateUserRequestHandler(getUserHelper());
+		return new CreateUserRequestHandler(userRepository);
 	}
 	
 	@Bean(name="updateUserRequestHandler")
 	@Scope("prototype")
 	public UpdateUserRequestHandler getUpdateUserRequestHandler()	{
-		return new UpdateUserRequestHandler(getUserHelper());
+		return new UpdateUserRequestHandler(userRepository);
 	}
 	
 	@Bean(name="deleteUserRequestHandler")
 	@Scope("prototype")
 	public DeleteUserRequestHandler getDeleteUserRequestHandler()	{
-		return new DeleteUserRequestHandler(getUserHelper());
+		return new DeleteUserRequestHandler(userRepository);
 	}
 	
 }
